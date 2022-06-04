a0b72e56bc22390647294eb2f5f4d09fe7ea5f80
Daniel <danlpsarcanjo@gmail.com>
2022-06-04 01:34:46 -0400

    Create auth user param decorator

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { UserDto } from 'src/user/dto/user.dto';
+import { User } from 'src/user/user.entity';
+import { AuthUser } from './auth-user.decorator';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Req() req: Request) {
-    return req.user;
+  async login(@AuthUser() authUser: User): Promise<UserDto> {
+    return authUser.dto;
   }
 }
