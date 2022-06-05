7ab34717c7d46c730c6a557005912b1c80a93578
Daniel Lopes Arcanjo <60639559+danlopes-arc@users.noreply.github.com>
2022-06-05 11:46:46 -0400

    #40 Add validation to update username endpoint (#54)
    * Add required constraint to user props
    
    * Create IsUnique validation rule
    
    * Add Validations
    
    * Add validation to UpdateUsernameDto

--- apps/server/src/user/dto/update-username.dto.ts
+++ apps/server/src/user/dto/update-username.dto.ts
@@ -1,7 +1,13 @@
 import { Exclude, Expose } from 'class-transformer';
+import { IsNotEmpty, MinLength } from 'class-validator';
+import { IsUnique } from '../../database-validation/unique.decorator';
+import { User } from '../user.entity';
 
 @Exclude()
 export class UpdateUsernameDto {
   @Expose()
+  @IsUnique(User)
+  @IsNotEmpty()
+  @MinLength(3)
   username!: string;
 }
