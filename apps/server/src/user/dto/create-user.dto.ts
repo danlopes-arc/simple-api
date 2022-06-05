976f099ba11f29dbc880e11b26eb0cfd5013faf9
Daniel <danlpsarcanjo@gmail.com>
2022-06-05 02:46:42 -0400

    Create IsUnique validation rule

--- apps/server/src/user/dto/create-user.dto.ts
+++ apps/server/src/user/dto/create-user.dto.ts
@@ -1,10 +1,13 @@
 import { Exclude, Expose } from 'class-transformer';
 import { IsNotEmpty } from 'class-validator';
+import { IsUnique } from '../../database-validation/unique.decorator';
+import { User } from '../user.entity';
 
 @Exclude()
 export class CreateUserDto {
   @Expose()
   @IsNotEmpty()
+  @IsUnique(User)
   username!: string;
 
   @Expose()
